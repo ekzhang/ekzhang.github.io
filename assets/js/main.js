60a6e0c13b23bda680710495538d3783b7a18d02
Eric Zhang <ekzhang1@gmail.com>
2020-01-16 19:37:52 -0600

    Add project placeholders

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,8 @@
     fetch(`https://api.github.com/repos/${repo}`)
       .then(r => r.json())
       .then(obj => {
-        el.innerHTML = obj.stargazers_count;
+        const stars = obj.stargazers_count;
+        if (stars) el.innerHTML = stars;
       });
   });
 })();
