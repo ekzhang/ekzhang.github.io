4a41bcc23e00e4ddd0371dcd28ce8475ef155ddd
Eric Zhang <ekzhang1@gmail.com>
2020-01-19 23:25:02 -0500

    Add smooth scrolling and ToC

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,17 @@
       });
   });
 })();
+
+(function anchorSmoothScroll() {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+      const elem = document.getElementById(
+        this.getAttribute("href").substring(1)
+      );
+      if (elem) {
+        elem.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+})();
