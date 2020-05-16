d75fa2bed7b28bb730329d048e25fceceb90db2e
Eric Zhang <ekzhang1@gmail.com>
2020-05-15 22:27:17 -0500

    Reduce GitHub API calls

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,28 @@
 (function populateStarCounts() {
+  const cache = {};
   const els = document.getElementsByClassName("star-count");
-  Array.prototype.forEach.call(els, el => {
-    const repo = el.dataset.repo;
-    fetch(`https://api.github.com/repos/${repo}`)
-      .then(r => r.json())
-      .then(obj => {
-        const stars = obj.stargazers_count;
-        if (stars) el.innerHTML = stars;
-      });
+  Array.prototype.forEach.call(els, (el) => {
+    const [user, name] = el.dataset.repo.split("/");
+    if (!cache.hasOwnProperty(user)) {
+      cache[user] = fetch(`https://api.github.com/users/${user}/repos`)
+        .then((r) => r.json())
+        .then((repoList) => {
+          const stars = {};
+          for (const obj of repoList) {
+            stars[obj.name] = obj.stargazers_count;
+          }
+          return stars;
+        });
+    }
+    cache[user].then((stars) => {
+      if (stars[name]) el.innerHTML = stars[name];
+    });
   });
 })();
 
 (function anchorSmoothScroll() {
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
       e.preventDefault();
       const elem = document.getElementById(
         this.getAttribute("href").substring(1)
