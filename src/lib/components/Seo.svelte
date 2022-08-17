d17026425dbd26ff602c70bfed80b0233d22ac0d
Eric Zhang <ekzhang1@gmail.com>
2022-08-17 11:43:56 -0400

    Change social image to Hudson landscape

--- src/lib/components/Seo.svelte
+++ src/lib/components/Seo.svelte
@@ -10,7 +10,7 @@
   <meta name="description" property="og:description" content={description} />
   <meta
     property="og:image"
-    content="https://www.ekzhang.com/assets/images/profile2.jpg"
+    content="https://www.ekzhang.com/assets/images/hudson-landscape.jpg"
   />
   <meta name="twitter:card" content="summary" />
 </svelte:head>
