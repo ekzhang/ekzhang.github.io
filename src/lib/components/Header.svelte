c9b6c1c7e41266f257f95af0b98882434b18438f
Eric Zhang <ekzhang1@gmail.com>
2022-09-06 21:51:29 -0400

    Add prefetch and noscroll nav link annotations

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -13,6 +13,8 @@
   <h1 class="font-serif text-[2.8rem] mt-5 mb-2 text-center">Eric Zhang</h1>
   <nav
     class="flex justify-center text-xl sm:text-2xl font-light mb-4 space-x-3"
+    data-sveltekit-prefetch
+    data-sveltekit-noscroll
   >
     {#each links as link (link)}
       <a
