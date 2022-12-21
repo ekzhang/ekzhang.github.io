a5a465eecb16bed7a2a2c261f9faacfe833d97da
Eric Zhang <ekzhang1@gmail.com>
2022-12-21 12:38:54 -0600

    Add navigation titles and simple 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  import { Bug } from "lucide-svelte";
+</script>
+
+<div class="layout-md">
+  <div
+    class="pt-14 pb-40 sm:pt-20 sm:pb-80 text-xl flex justify-center items-center"
+  >
+    <Bug class="mr-2" />
+    <span class="font-medium">{$page.status}</span>
+    <div class="mx-4 h-8 border-l border-black" />
+    {$page.error?.message}
+  </div>
+</div>
