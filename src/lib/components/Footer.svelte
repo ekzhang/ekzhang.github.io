beec9589fcd539b72fbf893a276195564a59986d
Eric Zhang <ekzhang1@gmail.com>
2022-12-21 11:39:14 -0600

    Make Footer.svelte

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,67 +1,31 @@
-<script lang="ts">
-  import Fa from "svelte-fa";
-  import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-  import {
-    faGithub,
-    faTwitter,
-    faFacebook,
-    faQuora,
-  } from "@fortawesome/free-brands-svg-icons";
-
-  const links = [
-    {
-      name: "GitHub",
-      href: "https://github.com/ekzhang",
-      icon: faGithub,
-      color: "#24292e",
-    },
-    {
-      name: "Twitter",
-      href: "https://twitter.com/ekzhang1",
-      icon: faTwitter,
-      color: "#00acee",
-    },
-    {
-      name: "Facebook",
-      href: "https://fb.me/ekzhang",
-      icon: faFacebook,
-      color: "#3b5998",
-    },
-    {
-      name: "Quora",
-      href: "https://www.quora.com/profile/Eric-Zhang-99",
-      icon: faQuora,
-      color: "#aa2200",
-    },
-    {
-      name: "Mail",
-      href: "mailto:ekzhang1@gmail.com",
-      icon: faEnvelope,
-      color: "#333",
-    },
-  ];
-</script>
-
-<!--
-<footer class="p-6 pb-8 space-y-4">
-  <div class="flex justify-center space-x-4">
-    {#each links as link (link)}
-      <a style="color: {link.color}" href={link.href}>
-        <Fa
-          icon={link.icon}
-          size="2x"
-          class="opacity-75 transition-opacity hover:opacity-100"
-        />
-      </a>
-    {/each}
-  </div>
-  <p class="text-center text-gray-400 font-light">
-    Made by Eric Zhang. Design inspired by Michael Fogleman.
-  </p>
-</footer> -->
-
 <footer class="mt-20 text-lg flex flex-col">
-  <div>
-    <p>Twitter</p>
+  <div class="row">
+    <span>Twitter</span>
+    <hr />
+    <a class="link" href="https://twitter.com/ekzhang1">@ekzhang1</a>
+  </div>
+  <div class="row">
+    <span>GitHub</span>
+    <hr />
+    <a class="link" href="https://github.com/ekzhang">@ekzhang</a>
+  </div>
+  <div class="row">
+    <span>Email</span>
+    <hr />
+    <a class="link" href="mailto:ekzhang1@gmail.com">ekzhang1@gmail.com</a>
   </div>
 </footer>
+
+<style lang="postcss">
+  .row {
+    @apply flex items-center space-x-4;
+  }
+
+  .row span {
+    @apply text-neutral-500;
+  }
+
+  .row hr {
+    @apply w-full mt-0.5 border-neutral-300 border-dotted;
+  }
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,25 +5,9 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    // container: {
-    //   center: true,
-    //   padding: {
-    //     DEFAULT: "1rem",
-    //     sm: "2rem",
-    //     md: "3rem",
-    //     lg: "4rem",
-    //     xl: "5rem",
-    //   },
-    //   screens: {
-    //     sm: "640px",
-    //     md: "768px",
-    //     lg: "1024px",
-    //     xl: "1280px",
-    //   },
-    // },
     extend: {
       fontFamily: {
-        sans: ["Helvetica Neue", "Helvetica", ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
         serif: ["Newsreader", ...defaultTheme.fontFamily.serif],
       },
     },
