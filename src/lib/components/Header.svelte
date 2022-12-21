7af5954c57900248c43673cad31d9f89c9685e1e
Eric Zhang <ekzhang1@gmail.com>
2022-12-21 12:49:55 -0600

    Clean responsive layout for the heading

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -19,22 +19,20 @@
 </script>
 
 <header
-  class="layout-md grid grid-cols-2"
+  class="layout-md flex justify-between items-start"
   data-sveltekit-noscroll
   data-sveltekit-preload-code="eager"
 >
   <h1 class="font-bold text-black text-2xl mb-6">
     <a href="/">Eric Zhang</a>
     {#if pageTitle}
-      <span class="font-light">
+      <span class="page-title">
         <span class="text-neutral-400">—</span>
         {pageTitle}
       </span>
     {/if}
   </h1>
-  <nav
-    class="flex items-start text-neutral-500 justify-end space-x-6 text-lg py-0.5"
-  >
+  <nav>
     {#each links as link (link)}
       <a
         href={link.href}
@@ -46,3 +44,29 @@
     {/each}
   </nav>
 </header>
+
+<style lang="postcss">
+  nav {
+    @apply flex items-start text-neutral-500 justify-end space-x-6 text-lg py-0.5;
+  }
+
+  .page-title {
+    @apply font-light;
+  }
+
+  @media (max-width: 580px) {
+    .page-title {
+      @apply block text-xl;
+    }
+
+    .page-title :first-child {
+      @apply hidden;
+    }
+  }
+
+  @media (max-width: 420px) {
+    nav {
+      @apply flex-col items-end space-x-0 mb-2;
+    }
+  }
+</style>
