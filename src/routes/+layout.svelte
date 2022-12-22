2b1772f4fd7ef353f5e6d3641ccf2196ee97cf9d
Eric Zhang <ekzhang1@gmail.com>
2022-12-22 14:14:43 -0600

    Add smooth page transitions to the site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,17 @@
   import "@fontsource/newsreader/400-italic.css";
   import "../app.css";
 
-  import { dev } from "$app/environment";
+  import { browser, dev } from "$app/environment";
+
+  import { fly } from "svelte/transition";
+
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import type { LayoutData } from "./$types";
+
+  export let data: LayoutData;
+
+  const isMobile = browser && /Android|iPhone/i.test(navigator.userAgent);
 </script>
 
 <svelte:head>
@@ -27,8 +35,20 @@
 
 <Header />
 
-<main>
-  <slot />
-</main>
+{#if isMobile}
+  <!-- Disable page transitions on mobile due to a browser engine bug. -->
+  <main>
+    <slot />
+  </main>
+{:else}
+  {#key data.pathname}
+    <main
+      in:fly={{ x: -10, duration: 350, delay: 350 }}
+      out:fly={{ y: 5, duration: 350 }}
+    >
+      <slot />
+    </main>
+  {/key}
+{/if}
 
 <Footer />
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,7 @@
+import type { LayoutLoad } from "./$types";
+
 export const prerender = true;
+
+export const load: LayoutLoad = async ({ url }) => {
+  return { pathname: url.pathname };
+};
